836b16d236347c4bc807a17c9ab79c2df8ad47df
Marc Templeton <marcte@kainos.com>
2022-03-30 16:57:26 +0100

    renaming dir to alerts to match repo

--- ffc-pay-alerts/index.js
+++ ffc-pay-alerts/index.js
@@ -1,6 +1,8 @@
 const validateMessage = require('./validate-message')
+const sendEmail = require('./notify')
 
 module.exports = async function (context, mySbMsg) {
   context.log('JavaScript ServiceBus topic trigger function received message', mySbMsg)
   await validateMessage(context, mySbMsg)
+  await sendEmail(context, mySbMsg)
 }
