eea634df15a2ae395956809554d9686dc1d401bc
Marc Templeton <marcte@kainos.com>
2022-04-04 14:50:24 +0100

    public env vars removed

--- test/env.js
+++ test/env.js
@@ -1,6 +1,6 @@
 module.exports = {
-  notifyApiKey: 'smoke_test_api_key-822632b6-1b07-44ef-9fe0-24dc9bb99272-45cce8e4-7daa-4233-89f3-7e53f7bde3e8',
-  notifyEmailTemplateId: 'ba96327f-d372-4589-90f5-e280a37c85fc',
+  notifyApiKey: 'NOTIFY TEST, TEAM OR LIVE API KEY STRING',
+  notifyEmailTemplateId: 'NOTIFY EMAIL TEMPLATE STRING',
   // https://docs.notifications.service.gov.uk/node.html#smoke-testing
   notifyEmailAddress: 'simulate-delivered@notifications.service.gov.uk'
 }
