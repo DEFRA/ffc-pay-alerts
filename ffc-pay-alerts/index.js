ef52d20eefadc7a48413036db254bd14518b2d42
Marc Templeton <marcte@kainos.com>
2022-04-22 15:43:05 +0100

    updating message variable name

--- ffc-pay-alerts/index.js
+++ ffc-pay-alerts/index.js
@@ -1,8 +1,8 @@
 const validateMessage = require('./validate-message')
 const { sendEmails } = require('./notify')
 
-module.exports = async (context, mySbMsg) => {
-  context.log('JavaScript ServiceBus topic trigger function received message', mySbMsg)
-  await validateMessage(context, mySbMsg)
-  await sendEmails(context, mySbMsg)
+module.exports = async (context, message) => {
+  context.log('JavaScript ServiceBus topic trigger function received message', message)
+  await validateMessage(context, message)
+  await sendEmails(context, message)
 }
