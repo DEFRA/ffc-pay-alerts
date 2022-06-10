6fe6fdc73793db543e68220b097154b56a8e44b0
Abidemi Adio <abidemi.adio@defra.gov.uk>
2022-06-10 09:07:58 +0100

    Added All existing notify event template

--- ffc-pay-alerts/notify/notifyTemplate/template-schema.js
+++ ffc-pay-alerts/notify/notifyTemplate/template-schema.js
@@ -9,4 +9,40 @@ module.exports = [{
 {
   eventType: 'payment-request-processing',
   notifyTemplateId: 'fb29affd-9493-467d-bdcf-7fb96463c15b'
+},
+{
+  eventType: 'dax-inbound',
+  notifyTemplateId: '0b1871ae-095d-4951-bf7e-1bea39a2c995'
+},
+{
+  eventType: 'dax-outbound',
+  notifyTemplateId: 'a95cf04d-b807-425e-bcbd-2e76397a4041'
+},
+{
+  eventType: 'payment-request-debt-response',
+  notifyTemplateId: 'e368d62c-105b-49fb-8b57-b23b5ce60e2c'
+},
+{
+  eventType: 'payment-request-enrichment',
+  notifyTemplateId: '3756efe8-d1a4-44aa-ba73-46666ce4dffe'
+},
+{
+  eventType: 'payment-request-ledger-assignment-quality-checked',
+  notifyTemplateId: '1ccac70c-6367-4864-b5df-420231535ee1'
+},
+{
+  eventType: 'payment-request-manual-ledger-response',
+  notifyTemplateId: 'b26da268-386a-4a89-b156-03afbe339de6'
+},
+{
+  eventType: 'payment-request-return',
+  notifyTemplateId: '46caee3f-64de-4c54-b47b-ee42f92800e4'
+},
+{
+  eventType: 'request-editor',
+  notifyTemplateId: '305d0be0-6e8b-47d0-a909-ce58c6693442'
+},
+{
+  eventType: 'siti-inbound',
+  notifyTemplateId: '48963d01-eb09-439c-a12e-e99129127b06'
 }]
