27f545436f3f0da7edf88413b98be87fd6b09955
Abidemi Adio <abidemi.adio@defra.gov.uk>
2022-06-10 10:03:04 +0100

    Fix for duplication

--- ffc-pay-alerts/notify/notify-template/template-schema.js
+++ ffc-pay-alerts/notify/notify-template/template-schema.js
@@ -1,8 +1,114 @@
 module.exports = [{
-  eventType: 'payment-request-submission',
-  notifyTemplateId: '982b92b2-da06-4c51-8996-33b13dd4ce04'
+  eventType: 'siti-inbound',
+  service: 'siti inbound',
+  externalProcess: true,
+  eventLinks: [
+    {
+      link: 'batch-processing'
+    }
+  ]
+},
+{
+  eventType: 'dax-inbound',
+  service: 'dax inbound',
+  externalProcess: true,
+  eventLinks: [
+    {
+      link: 'payment-request-return'
+    }
+  ]
+},
+{
+  eventType: 'dax-outbound',
+  externalProcess: true,
+  service: 'dax outbound',
+  eventLinks: []
 },
 {
   eventType: 'batch-processing',
-  notifyTemplateId: '6645e4aa-aec9-48d9-a674-aa3b15e9cebb'
+  service: 'ffc-pay-batch-processor',
+  eventLinks: [
+    {
+      link: 'payment-request-enrichment'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-enrichment',
+  service: 'ffc-pay-enrichment',
+  eventLinks: [
+    {
+      link: 'payment-request-processing'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-processing',
+  service: 'ffc-pay-processing',
+  eventLinks: [
+    {
+      link: 'payment-request-submission'
+    },
+    {
+      link: 'payment-request-debt-response'
+    },
+    {
+      link: 'payment-request-manual-ledger-response'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-submission',
+  service: 'ffc-pay-submission',
+  notifyTemplateId: '982b92b2-da06-4c51-8996-33b13dd4ce04',
+  eventLinks: [
+    {
+      link: 'dax-outbound'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-debt-response',
+  service: 'debt-enrichment',
+  eventLinks: [
+    {
+      link: 'request-editor'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-manual-ledger-response',
+  service: 'manual-ledger-check',
+  eventLinks: [
+    {
+      link: 'request-editor'
+    }
+  ]
+},
+{
+  eventType: 'request-editor',
+  service: 'ffc-pay-request-editor',
+  eventLinks: [
+    {
+      link: 'payment-request-ledger-assignment-quality-checked'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-ledger-assignment-quality-checked',
+  service: 'quality-check',
+  eventLinks: [
+    {
+      link: 'payment-request-processing'
+    }
+  ]
+},
+{
+  eventType: 'payment-request-return',
+  service: 'ffc-pay-responses',
+  eventLinks: [
+    {
+      link: 'payment-request-processing'
+    }
+  ]
 }]
