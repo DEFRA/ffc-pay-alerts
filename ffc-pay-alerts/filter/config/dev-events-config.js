d3c10b50a0d012aadb3064f62823b7e264f7d1fb
Simon Dunn <simondunn90@gmail.com>
2022-07-07 11:57:19 +0100

    refactor tests

--- ffc-pay-alerts/filter/config/dev-events-config.js
+++ ffc-pay-alerts/filter/config/dev-events-config.js
@@ -1,4 +1,6 @@
 module.exports = [
+  'mock-event-name',
+  'dev-mock-event',
   'batch-processing-quarantine-error',
   'batch-processing-error',
   'payment-request-blocked',
--- ffc-pay-alerts/filter/config/rpa-events-config.js
+++ ffc-pay-alerts/filter/config/rpa-events-config.js
@@ -1,4 +1,5 @@
 module.exports = [
   'mock-event-name',
+  'rpa-mock-event',
   'payment-request-blocked'
 ]
