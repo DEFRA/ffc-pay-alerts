44a9b81f678d7ea1b338179d5d149fa58597426e
kirsten.williamson <kirsten.williamson@kainos.com>
2023-01-27 13:43:54 +0000

    Test commit

--- ffc-pay-alerts/notify/send-emails.js
+++ ffc-pay-alerts/notify/send-emails.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid')
 const sendEmail = require('./send-email')
 
 const sendEmails = async (context, message, emailAddresses = notifyEmailAddresses, reference = uuidv4()) => {
-  // process.env.NOTIFY_EMAIL_ADDRESSES returns string, so splitting by comma to get array
+  // process.env.NOTIFY_EMAIL_ADDRESSES returns string, so splitting by comma to get an array
   const recipients = typeof (emailAddresses) === 'string' ? formatEmailAddresses(emailAddresses) : emailAddresses
 
   for (const recipient of recipients) {
