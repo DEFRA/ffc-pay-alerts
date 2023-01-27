c5ab8d7d2a343a88e739eed0f9d22c95d7f1a464
kirsten.williamson <kirsten.williamson@kainos.com>
2023-01-27 11:43:45 +0000

    Updated .replaceAll with .replace

--- ffc-pay-alerts/notify/format-email-addresses.js
+++ ffc-pay-alerts/notify/format-email-addresses.js
@@ -3,8 +3,7 @@ const formatEmailAddresses = (emailAddresses) => {
     // process.env.NOTIFY_EMAIL_ADDRESSES returns string, so splitting by comma to get array
     emailAddresses = emailAddresses.split(',')
   }
-  // Problem child
-  return emailAddresses.map(email => email.replaceAll(' ', ''))
+  return emailAddresses.map(email => email.replace(/^\s+|\s+$/gm, ''))
 }
 
 module.exports = formatEmailAddresses
